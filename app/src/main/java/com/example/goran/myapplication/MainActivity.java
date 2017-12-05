83eef637e8533e561a831348c78325f288d8d0fc
goran <gogovoinovski@gmail.com>
2017-12-05 18:53:48 +0100

    pet

--- app/src/main/java/com/example/goran/myapplication/MainActivity.java
+++ app/src/main/java/com/example/goran/myapplication/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.goran.myapplication;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.pager)ViewPager mojpager;
+    @BindView(R.id.tablayout)TabLayout mojtablayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+        setUpViewPager(mojpager);
 
+    }
 
+    private void setUpViewPager(ViewPager mojpager) {
 
+        MyPageAdapter adapter = new MyPageAdapter(this.getSupportFragmentManager());
 
+        mojpager.setAdapter(adapter);
     }
 }
